5682786b65029c7a426c0f44e091edde2681acb3
Arman-Kabir <armankabir8@gmail.com>
2024-04-29 09:36:56 +0600

    mui installed and icons added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,22 @@
+import Logo from "../assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div>Navbar</div>;
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/notes/notes.js
@@ -0,0 +1,8 @@
+
+/** Libraries installation
+ * ##react-icons -> https://react-icons.github.io/react-icons/
+ * 
+ * ##mui,icons --> https://mui.com/material-ui/getting-started/installation/
+ * 
+ * 
+ */

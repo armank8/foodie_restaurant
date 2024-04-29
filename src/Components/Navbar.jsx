7e27a3e7347a6b595da445bc6d9613af3fe11aec
Arman-Kabir <armankabir8@gmail.com>
2024-04-29 09:58:15 +0600

    navbar static feature done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,12 +11,58 @@ import {
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
-import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
-import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
-import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import { useState } from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon></HomeIcon>,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon></InfoIcon>,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon></CommentRoundedIcon>,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon></PhoneRoundedIcon>,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon></ShoppingCartRoundedIcon>,
+    },
+  ];
+
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonials</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon"></BsCart2>
+        </a>
+        <button className="primary-button">Bookings Now</button>
+      </div>
+
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)}></HiOutlineBars3>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
